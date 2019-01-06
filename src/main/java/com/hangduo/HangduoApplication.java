8c9d0c8e0f09980f06397e7465e1cea4bcbdc85a
YxYFrist <1612219848@qq.com>
2019-01-06 17:36:35 +0800

    yxy

--- src/main/java/com/hangduo/HangduoApplication.java
+++ src/main/java/com/hangduo/HangduoApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HangduoApplication {
-//
-    //
     public static void main(String[] args) {
         SpringApplication.run(HangduoApplication.class, args);
     }
---
+++ src/main/java/com/hangduo/entity/statute.java
@@ -0,0 +1,7 @@
+package com.hangduo.entity;
+
+public class statute {
+
+    private Integer user;
+    private  String demo;
+}
