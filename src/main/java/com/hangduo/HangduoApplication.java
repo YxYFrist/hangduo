6e25dd700502de9f5cdbfddb74ea55b98e22a550
YxYFrist <1612219848@qq.com>
2019-01-06 17:22:12 +0800

    kkkk

---
+++ src/main/java/com/hangduo/HangduoApplication.java
@@ -0,0 +1,14 @@
+package com.hangduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangduoApplication {
+//
+    public static void main(String[] args) {
+        SpringApplication.run(HangduoApplication.class, args);
+    }
+
+}
+
